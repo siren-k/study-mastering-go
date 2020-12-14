55084cde61d1904b1c55273da5b9f1e954ad188e
Siren K <siren.k.pro@gmail.com>
2020-12-15 06:27:02 +0900

    log.Fatal 예제

--- chapter_01/logFatal.go
+++ chapter_01/logFatal.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"log/syslog"
+)
+
 func main() {
-	$END$
+	sysLog, err := syslog.New(syslog.LOG_ALERT|syslog.LOG_MAIL, "Some program!")
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.SetOutput(sysLog)
+	}
+
+	log.Fatal(sysLog) // 정말 나쁜 일이 발생해서 상황을 알려주자마자 프로그램을 종료하고 싶을 때 사용
+	fmt.Println("Will you see this?")
 }
