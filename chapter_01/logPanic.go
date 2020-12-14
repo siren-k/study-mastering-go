6bed469f6efbab7bcdf13e591311ac9fea20ef85
Siren K <siren.k.pro@gmail.com>
2020-12-15 06:34:09 +0900

    log.Panic() 예제

---
+++ chapter_01/logPanic.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"log/syslog"
+)
+
+func main() {
+	sysLog, err := syslog.New(syslog.LOG_ALERT|syslog.LOG_MAIL, "Some program!")
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		log.SetOutput(sysLog)
+	}
+
+	log.Panic(sysLog) // 프로그램이 다시 실행될 수 없을 정도로 오류가 발생하는 순간, 이에 관련된 정보를 최대한 알고 싶을 때 사용
+	// log.Fatal()과 마찬가지로 적절한 로그 파일에 항목을 추가한 뒤에 곧바로 프로그램을 종료
+	fmt.Println("Will you see this?")
+}
