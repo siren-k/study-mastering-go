fa25e7d80297c4646e439508ec2e0cb4ac690e8a
Siren K <siren.k.pro@gmail.com>
2020-12-30 11:51:45 +0900

    HTTP 핸들러 테스트 예제

--- chapter_12/kvWeb/kvWeb.go
+++ chapter_12/kvWeb/kvWeb.go
@@ -103,18 +103,18 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func listAll(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Listing the contents of the KV store!")
-	fmt.Fprintf(w, "<a href=\"/\" style=\"margin-right: 20px;\">Home sweet home!</a>")
-	fmt.Fprintf(w, "<a href=\"/list\" style=\"margin-right: 20px;\">List all elements!</a>")
-	fmt.Fprintf(w, "<a href=\"/change\" style=\"margin-right: 20px;\">Change an element!</a>")
-	fmt.Fprintf(w, "<a href=\"/insert\" style=\"margin-right: 20px;\">Insert new element!</a>")
-	fmt.Fprintf(w, "<h1>The contents of the KV strore are:</h1>")
-	fmt.Fprintf(w, "<ul>")
+	fmt.Fprintf(w, `<a href="/" style="margin-right: 20px;">Home sweet home!</a>`)
+	fmt.Fprintf(w, `<a href="/list" style="margin-right: 20px;">List all elements!</a>`)
+	fmt.Fprintf(w, `<a href="/change" style="margin-right: 20px;">Change an element!</a>`)
+	fmt.Fprintf(w, `<a href="/insert" style="margin-right: 20px;">Insert new element!</a>`)
+	fmt.Fprintf(w, `<h1>The contents of the KV strore are:</h1>`)
+	fmt.Fprintf(w, `<ul>`)
 	for k, v := range DATA {
-		fmt.Fprintf(w, "<li>")
-		fmt.Fprintf(w, "<strong>%s</strong> with value: %v\n", k, v)
-		fmt.Fprintf(w, "</li>")
+		fmt.Fprintf(w, `<li>`)
+		fmt.Fprintf(w, `<strong>%s</strong> with value: %v\n`, k, v)
+		fmt.Fprintf(w, `</li>`)
 	}
-	fmt.Fprintf(w, "</ul>")
+	fmt.Fprintf(w, `</ul>`)
 }
 
 func changeElement(w http.ResponseWriter, r *http.Request) {
---
+++ chapter_12/testWWW/testWWW_test.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCheckStatusOK(t *testing.T) {
+	req, err := http.NewRequest("GET", "/CheckStatusOK", nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(CheckStatusOK)
+	handler.ServeHTTP(rr, req)
+
+	status := rr.Code
+	if status != http.StatusOK {
+		t.Errorf("handler returned %v", status)
+	}
+
+	expect := `Fine!`
+	if rr.Body.String() != expect {
+		t.Errorf("handler returned %v", rr.Body.String())
+	}
+}
+
+func TestStatusNotFound(t *testing.T) {
+	req, err := http.NewRequest("GET", "/StatusNotFound", nil)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(StatusNotFound)
+	handler.ServeHTTP(rr, req)
+
+	status := rr.Code
+	if status != http.StatusNotFound {
+		t.Errorf("handler returnd %v", status)
+	}
+}
+
+// ❯ go test *.go -v
+// === RUN   TestCheckStatusOK
+// --- PASS: TestCheckStatusOK (0.00s)
+// === RUN   TestStatusNotFound
+// --- PASS: TestStatusNotFound (0.00s)
+// PASS
+// ok      command-line-arguments  0.128s
